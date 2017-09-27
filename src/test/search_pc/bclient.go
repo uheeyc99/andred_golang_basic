cc6f9a6e7d2074728fb2449347a860e286926027
Eric Gao <genghis.bupt@gmail.com>
2017-09-27 21:22:28 +0800

    a

---
+++ src/test/search_pc/bclient.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"net"
+	"fmt"
+
+	"strconv"
+
+	"os"
+
+	"time"
+)
+
+
+var  peer_port int = 65535
+var  local_port int = 65530
+func get_internal()(str string) {
+	var ip_string string
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		os.Stderr.WriteString("Oops:" + err.Error())
+		os.Exit(1)
+	}
+	for _, a := range addrs {
+		if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				//os.Stdout.WriteString(ipnet.IP.String() + "\n")
+				ip_string = ipnet.IP.String()
+			}
+		}
+	}
+	return ip_string
+}
+
+func Ask(){
+
+	rAddr,err:=net.ResolveUDPAddr("udp4","255.255.255.255:65535")
+	if(nil !=err){
+		fmt.Println(err)
+		fmt.Println("dd")
+		return
+	}
+
+	conn1,err:=net.DialUDP("udp",nil,rAddr)
+	if(nil !=err){
+		fmt.Println(err)
+		fmt.Println("ss")
+		return
+	}
+	defer conn1.Close()
+
+	_,err=conn1.Write([]byte(strconv.Itoa(local_port)))
+	if(nil !=err){
+		fmt.Println(err)
+		fmt.Println("aa")
+		return
+	}
+
+
+
+	udpaddr,err:=net.ResolveUDPAddr("udp4",":"+strconv.Itoa(local_port))
+	if(nil !=err){
+		fmt.Println(err)
+		return
+	}
+	conn2,err:=net.ListenUDP("udp",udpaddr)
+	if(nil !=err){
+		fmt.Println(err)
+		return
+	}
+	defer conn2.Close()
+	var buf [1024]byte
+
+	for i:=0;i<10;i++{
+		conn2.SetReadDeadline(time.Now().Add( time.Second))
+		//time.Now().Add(time.Duration(10) * time.Second)
+		n,raddr,err:=conn2.ReadFromUDP(buf[0:])
+		if(err !=nil){
+			fmt.Println(err)
+			continue
+		}
+		fmt.Println("received response:" + string(buf[0:n])+" from "+ raddr.IP.String(),strconv.Itoa(raddr.Port))
+	}
+
+
+}
+
+
+
+func main()  {
+
+		for i:=0;i<20;i++{
+
+			Ask()
+		}
+
+
+}
--- src/test/udp_client/udp_client.go
+++ src/test/udp_client/udp_client.go
@@ -5,11 +5,14 @@ import (
 	"fmt"
 	"time"
 	"strconv"
+	"net/http"
+	"os"
+	"io"
 )
 
 func Ask(i int){
 
-	rAddr,err:=net.ResolveUDPAddr("udp4","10.10.3.226:60000")
+	rAddr,err:=net.ResolveUDPAddr("udp4","127.0.0.1:65535")
 	if(nil !=err){
 		fmt.Println(err)
 		fmt.Println("dd")
@@ -31,7 +34,7 @@ func Ask(i int){
 		return
 	}
 	var buf [1024]byte
-	_,_,err=conn.ReadFromUDP(buf[0:])
+	n,_,err:=conn.ReadFromUDP(buf[0:])
 	if(nil !=err){
 		fmt.Println(err)
 		//fmt.Println("uu")
@@ -42,7 +45,40 @@ func Ask(i int){
 
 }
 
+func get_external(){
+	resp, err := http.Get("http://myexternalip.com/raw")
+	if err != nil {
+		os.Stderr.WriteString(err.Error())
+		os.Stderr.WriteString("\n")
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+	io.Copy(os.Stdout, resp.Body)
+}
+
+func get_internal()(str string) {
+	var ip_string string
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		os.Stderr.WriteString("Oops:" + err.Error())
+		os.Exit(1)
+	}
+	for _, a := range addrs {
+		if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				//os.Stdout.WriteString(ipnet.IP.String() + "\n")
+				ip_string = ipnet.IP.String()
+			}
+		}
+	}
+	return ip_string
+}
+
+
 func main()  {
+
+	fmt.Println(get_internal())
+
 	t1:=time.Now()
 	fmt.Println(t1)
 	for i:=0;i<1000;i++{
