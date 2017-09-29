9cfb4af615336480058c29c34263b49817e7d3f5
Eric Gao <genghis.bupt@gmail.com>
2017-09-30 07:26:06 +0800

    a

---
+++ src/test/goroutine/channel.go
@@ -0,0 +1,28 @@
+package main
+
+
+func test_channel_01(){
+	// channel
+}
+
+func test_channel_02(){
+	//buffered channel
+}
+
+func test_channel_03(){
+	// channel数组
+}
+
+func test_gosched(){
+	//让出时间片
+}
+
+func test_waitgroup(){
+	//var w sync.WaitGroup
+}
+
+
+
+func main(){
+
+}
