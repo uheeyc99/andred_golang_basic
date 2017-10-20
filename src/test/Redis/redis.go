c20b9bc134614cf909b60ba5191cb05cb80422db
Eric Gao <genghis.bupt@gmail.com>
2017-10-21 00:46:58 +0800

    jwt

--- src/test/Redis/redis.go
+++ src/test/Redis/redis.go
@@ -1,1 +1,47 @@
-package Redis
+package main
+
+import (
+	"fmt"
+	"github.com/garyburd/redigo/redis"
+	"time"
+)
+
+func run_redis(){
+
+
+	c,e:=redis.Dial("tcp","qycam.com:50218")
+	if e!=nil{
+		fmt.Println(e)
+		return
+	}
+	defer c.Close()
+
+	_,err0:=c.Do("AUTH","andrew")
+	if err0!=nil {
+		fmt.Println(err0)
+	}
+	//fmt.Println("pass:",v0)
+
+
+
+	_,err:=c.Do("set","color1","red")
+	if err!=nil {
+		fmt.Println(err)
+	}
+	//fmt.Println("set :",v)
+
+	if err!=nil {
+		fmt.Println(err)
+	}
+	_,err=redis.String(c.Do("get","color1"))
+//fmt.Println("get value:",v)
+
+
+
+}
+
+func main(){
+	t:=time.Now()
+	run_redis()
+	fmt.Println(time.Since(t))
+}
